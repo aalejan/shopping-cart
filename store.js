1bad3d92b52635f883f08684de0d3fff4248bb69
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-04-29 13:19:47 -0400

    rendered price and category

--- store.js
+++ store.js
@@ -17,9 +17,14 @@ const name = storeItem.querySelector('[data-name]')
 name.innerText = item.name 
 
 
-
 const image = storeItem.querySelector('[data-image')
 image.src = `https://dummyimage.com/210x130/${item.imageColor}/${item.imageColor}`
+
+const catagory = storeItem.querySelector('[data-catagory]')
+catagory.innerText = item.category
+
+const price = storeItem.querySelector('[data-price]')
+price.innerText = `$${(item.priceCents / 100).toFixed(2)}`
 
 storeItemContainer.appendChild(storeItem)
 }
