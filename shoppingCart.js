84fa33c14c532966f7bfd570f2c38430f3cc8b3b
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-05-12 19:22:09 -0400

    quantity changes if multiple items of same id are added to cart

--- shoppingCart.js
+++ shoppingCart.js
@@ -14,14 +14,20 @@ export function setupShoppingCart(){}
  })
 
  export function addToCart(id){
+    const existingItem = shoppingCart.find(entry => entry.id === id )
+    if(existingItem){
+        existingItem.quantity++
+    }else{
+        shoppingCart.push({id: id, quantity: 1})
+    }
 
-    shoppingCart.push({id: id, quantity: 1})
+    
     console.log(shoppingCart)
     renderCart()
 }
 
  function renderCart(){
-     
+     cartItemsContainer.innerHTML = ''
     shoppingCart.forEach(entry => {
         
         const item = items.find( i => entry.id === i.id)
@@ -37,8 +43,11 @@ name.innerText = item.name
 const image = cartItem.querySelector('[data-image]')
 image.src = `https://dummyimage.com/210x130/${item.imageColor}/${item.imageColor}`
 
-const quantity = cartItem.querySelector('[data-quantity]')
-quantity.innerText = `x${entry.quantity}` 
+if(entry.quantity > 1) {
+    const quantity = cartItem.querySelector('[data-quantity]')
+    quantity.innerText = `x${entry.quantity}` 
+}
+
 
 const price = cartItem.querySelector('[data-price]')
 price.innerText = `$${((item.priceCents / 100) * entry.quantity).toFixed(2)}`
